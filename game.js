a6a39502147459c2ae3b3939b70862fdbbc7f9a2
Brap098 <Brapagon.Work@gmail.com>
2022-06-07 21:24:42 -0400

    ship move,colors

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
 // Frames per secound
 const FPS=35; 
+const Slow = 0.9; // Ship slowes over time with no thrust
+const Thrust_Speed = 6; // acceleration of the ship in pixels per second per second
 const SHIP_SIZE = 30; // ship height px
-const Turn_Speed = 360; // turn speed degrees(dps)
+const Turn_Speed = 400; // turn speed degrees(dps)
 
 
 /** @type {HTMLCanvasElements} */
@@ -61,9 +63,43 @@ function update(){
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canv.width, canv.height);
 
+    
+
+
+    //Thrust/move
+    if (ship.thrusting) {
+        ship.thrust.x += Thrust_Speed * Math.cos(ship.a) / FPS;
+        ship.thrust.y -= Thrust_Speed * Math.sin(ship.a) / FPS;
+        
+                        // draw the thruster
+                        ctx.fillStyle = "#0ECBFF";
+                        ctx.strokeStyle = "#C422D6";
+                        ctx.lineWidth = SHIP_SIZE / 10;
+                        ctx.beginPath();
+                        ctx.moveTo( // rear left
+                            ship.x - ship.r * (2 / 3 * Math.cos(ship.a) + 0.5 * Math.sin(ship.a)),
+                            ship.y + ship.r * (2 / 3 * Math.sin(ship.a) - 0.5 * Math.cos(ship.a))
+                        );
+                        ctx.lineTo( // rear centre (behind the ship)
+                            ship.x - ship.r * 5 / 3 * Math.cos(ship.a),
+                            ship.y + ship.r * 5 / 3 * Math.sin(ship.a)
+                        );
+                        ctx.lineTo( // rear right
+                            ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - 0.5 * Math.sin(ship.a)),
+                            ship.y + ship.r * (2 / 3 * Math.sin(ship.a) + 0.5 * Math.cos(ship.a))
+                        );
+                        ctx.closePath();
+                        ctx.fill();
+                        ctx.stroke();
+    } else{
+        //apply friction (slow the ship down when not thrusting)               
+        ship.thrust.x -= Slow * ship.thrust.x / FPS;               
+        ship.thrust.y -= Slow * ship.thrust.y / FPS;
+    }
+
     //make ship
- ctx.strokeStyle = "limeGreen";
-       ctx.lineWidth = SHIP_SIZE / 20;
+ ctx.strokeStyle = "#05F70D";
+       ctx.lineWidth = SHIP_SIZE / 15;
        ctx.beginPath();
        ctx.moveTo( // nose of the ship
            ship.x + 4 / 3 * ship.r * Math.cos(ship.a),
@@ -82,12 +118,24 @@ function update(){
 
     //rotate ship
     ship.a += ship.rot
-
+    //loop space(ship will apear on opposite side of side flown through)
+    if (ship.x < 0 - ship.r) {
+        ship.x = canv.width + ship.r;
+    }else if(ship.x > canv.width + ship.r ){
+        ship.x =  0 - ship.r;
+    }
+    if (ship.y< 0 - ship.r) {
+        ship.y = canv.height + ship.r;
+    }else if(ship.y> canv.height + ship.r ){
+        ship.y =  0 - ship.r;
+    }
 
     //move ship
-
+    ship.x += ship.thrust.x;
+    ship.y += ship.thrust.y;
+ 
     //center
-    ctx.fillStyle = "blue";
-    ctx.fillRect(ship.x - 1, ship.y - 1, 2,2);
+    ctx.fillStyle = "#F511AD";
+    //ctx.fillRect(ship.x - 1, ship.y - 1, 2.5,2.5);
 }
 
